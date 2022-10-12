24e03d4b43988e01cff13852b73ac05a2b60c3d0
Tiffany Conroy <tiffany@soundcloud.com>
2022-10-12 13:06:15 +0200

    Exploring Poisson distributions

--- writings/queue-illustrations.js
+++ writings/queue-illustrations.js
@@ -99,8 +99,45 @@ function genRandomTime(avgTime) {
     // generate a random number of seconds between 0 and infinity,
     // where avgTime is the most likely but erm... could be anything
 
+    // The results will fall into a Poisson distribution.
+    // A Poisson distribution is a histogram of the expected number of discrete events
+    // that are likely to offer in a given time frame.
+    // From that perspective, we could generate the expected # of people
+    // to arrive in a given clock tick, but this is not how we can generate
+    // a random arrival _interval_
+    // https://en.wikipedia.org/wiki/Poisson_distribution
+    // http://web.tecnico.ulisboa.pt/mcasquilho/acad/or/queue/SBakerQueuingI.pdf
+    // https://gist.github.com/ferreiro/2b5caac126b58bebce82
+
     // for now, return a random value equally distributed between 0 and twice times the avgTime.
     return Math.round(avgTime * 2 * Math.random());
+}
+
+// returns numberOfWindows counts in intervals of duration 5 * avgArrivalInterval
+function arrivals(avgArrivalInterval, numberOfWindows) {
+
+    const avgCountsPerInterval = 1 / avgArrivalInterval;
+    const observationWindowInIntervals = 5 * avgArrivalInterval;
+    const window = observationWindowInIntervals;
+
+    console.log(`average in ${window} intervals:`, avgCountsPerInterval * window);
+
+    let sum = 0.5 // average value after sum -= 5
+    let tbl = [];
+
+    for (let i = 0; i < numberOfWindows; i++) {
+        let count = 0;
+        let m;
+        while (sum < window) {
+             if (count > 1000) { break; }; // safe guard from typos
+            count += 1;
+            sum += (-Math.log(Math.random())) / avgCountsPerInterval;
+        }
+        tbl.push(count);
+        sum -= window;
+    }
+
+    return tbl;
 }
 
 document.getElementById("start-simulation").addEventListener("click", simulate);
