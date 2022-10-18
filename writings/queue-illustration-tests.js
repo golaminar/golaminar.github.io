e9f0f0720f182eecc5ee8521c36bde2e5301b747
Tiffany Conroy <tiffany@soundcloud.com>
2022-10-18 10:47:58 +0200

    Confine the fixtures within the scope of the test

--- writings/queue-illustration-tests.js
+++ writings/queue-illustration-tests.js
@@ -1,11 +1,17 @@
 const { isDeepEqual } = require("./test-helpers");
-const { arrivalTimes, serviceTimes, outputFixture } = require("./pre-calculated-intervals");
 
 // this is the tests!!
 console.log("this is the tests!!");
 
-const cumulativeArrivalTimes = computeCumulativeArrivalTimes(arrivalTimes);
-const serviceEndTimes = computeServiceEndTimes(serviceTimes, cumulativeArrivalTimes);
-const queueChangesPerTick = computeQueueChangesPerTick(cumulativeArrivalTimes, serviceEndTimes, 600);
+(function () {
 
-console.log(isDeepEqual(queueChangesPerTick, outputFixture));
+    const { arrivalTimes, serviceTimes, outputFixture } = require("./pre-calculated-intervals");
+
+    const cumulativeArrivalTimes = computeCumulativeArrivalTimes(arrivalTimes);
+    const serviceEndTimes = computeServiceEndTimes(serviceTimes, cumulativeArrivalTimes);
+    const queueChangesPerTick = computeQueueChangesPerTick(cumulativeArrivalTimes, serviceEndTimes, 600);
+
+    console.log(isDeepEqual(queueChangesPerTick, outputFixture));
+
+})();
+
