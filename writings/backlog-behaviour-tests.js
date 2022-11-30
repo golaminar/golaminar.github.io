8b87fe296f61613ce97cc68ca12af4280c0217a5
Tiffany Conroy <tiffany@soundcloud.com>
2022-11-30 15:19:19 +0100

    updated backlog expectations

--- writings/backlog-behaviour-tests.js
+++ writings/backlog-behaviour-tests.js
@@ -11,23 +11,21 @@ console.log("These are the backlog behaviour tests:\n-----------------");
     const unboundedBacklogExpectation = [
         { arrived: 5, capacity: 5, done: 5, backlogSizeAfter: 0 }, // iteration 1
         { arrived: 5, capacity: 4, done: 4, backlogSizeAfter: 1 }, // iteration 2
-        { arrived: 6, capacity: 5, done: 5, backlogSizeAfter: 2 }, // iteration 3
-        { arrived: 6, capacity: 6, done: 6, backlogSizeAfter: 2 }, // iteration 4
-        { arrived: 4, capacity: 5, done: 5, backlogSizeAfter: 1 }, // iteration 5
+        { arrived: 6, capacity: 4, done: 4, backlogSizeAfter: 3 }, // iteration 3
+        { arrived: 6, capacity: 6, done: 6, backlogSizeAfter: 3 }, // iteration 4
+        { arrived: 5, capacity: 6, done: 6, backlogSizeAfter: 2 }, // iteration 5
     ];
 
     const boundedBacklogExpectation = [
         { arrived: 5, capacity: 5, done: 5, backlogSizeAfter: 0 }, // iteration 1
         { arrived: 5, capacity: 4, done: 4, backlogSizeAfter: 1 }, // iteration 2
-        { arrived: 6, capacity: 5, done: 5, backlogSizeAfter: 1 }, // iteration 3
+        { arrived: 6, capacity: 4, done: 4, backlogSizeAfter: 2 }, // iteration 3
         { arrived: 6, capacity: 6, done: 6, backlogSizeAfter: 0 }, // iteration 4
-        { arrived: 4, capacity: 5, done: 4, backlogSizeAfter: 0 }, // iteration 5
+        { arrived: 5, capacity: 6, done: 5, backlogSizeAfter: 0 }, // iteration 5
     ];
 
     const unboundedBacklog = computeBacklogBehaviour(iterationArrivals, iterationCapacities);
     const boundedBacklog = computeBacklogBehaviour(iterationArrivals, iterationCapacities, 6);
-
-    console.log(unboundedBacklog, boundedBacklog)
 
     console.log("correctly computes unbounded backlog behaviour", isDeepEqual(unboundedBacklog, unboundedBacklogExpectation));
     console.log("correctly computes bounded backlog behaviour", isDeepEqual(boundedBacklog, boundedBacklogExpectation));
