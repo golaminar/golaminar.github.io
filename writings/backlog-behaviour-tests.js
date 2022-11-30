ec028f4f1f1a76afb0f79e544f7d5cb0897a6d94
Tiffany Conroy <tiffany@soundcloud.com>
2022-11-30 15:07:39 +0100

    Set up an example that illustrates the key ideas

--- writings/backlog-behaviour-tests.js
+++ writings/backlog-behaviour-tests.js
@@ -5,103 +5,29 @@ console.log("These are the backlog behaviour tests:\n-----------------");
 
 (function () {
 
-    const iterationArrivals = [5, 5, 6, 6, 4, 4];
-    const iterationCapacities = [5, 4, 5, 6, 5, 7];
+    const iterationArrivals = [5, 5, 6, 6, 5];
+    const iterationCapacities = [5, 4, 4, 6, 6];
 
     const unboundedBacklogExpectation = [
-        {
-            // iteration 1
-            arrived: 5,
-            capacity: 5,
-            done: 5,
-            backlogSizeAfter: 0,
-        },
-        {
-            // iteration 2
-            arrived: 5,
-            capacity: 4,
-            done: 4,
-            backlogSizeAfter: 1,
-        },
-        {
-            // iteration 3
-            arrived: 6,
-            capacity: 6,
-            done: 6,
-            backlogSizeAfter: 3,
-        },
-        {
-            // iteration 4
-            arrived: 6,
-            capacity: 6,
-            done: 6,
-            backlogSizeAfter: 3,
-        },
-        {
-            // iteration 5
-            arrived: 4,
-            capacity: 5,
-            done: 5,
-            backlogSizeAfter: 2,
-        },
-        {
-            // iteration 6
-            arrived: 4,
-            capacity: 7,
-            done: 6,
-            backlogSizeAfter: 0,
-        },
+        { arrived: 5, capacity: 5, done: 5, backlogSizeAfter: 0 }, // iteration 1
+        { arrived: 5, capacity: 4, done: 4, backlogSizeAfter: 1 }, // iteration 2
+        { arrived: 6, capacity: 5, done: 5, backlogSizeAfter: 2 }, // iteration 3
+        { arrived: 6, capacity: 6, done: 6, backlogSizeAfter: 2 }, // iteration 4
+        { arrived: 4, capacity: 5, done: 5, backlogSizeAfter: 1 }, // iteration 5
     ];
 
     const boundedBacklogExpectation = [
-        {
-            // iteration 1
-            arrived: 5,
-            capacity: 5,
-            done: 5,
-            backlogSizeAfter: 0,
-        },
-        {
-            // iteration 2
-            arrived: 5,
-            capacity: 4,
-            done: 4,
-            backlogSizeAfter: 1,
-        },
-        {
-            // iteration 3
-            arrived: 6,
-            capacity: 6,
-            done: 6,
-            backlogSizeAfter: 2,
-        },
-        {
-            // iteration 4
-            arrived: 6,
-            capacity: 6,
-            done: 6,
-            backlogSizeAfter: 3,
-        },
-        {
-            // iteration 5
-            arrived: 4,
-            capacity: 5,
-            done: 5,
-            backlogSizeAfter: 1,
-        },
-        {
-            // iteration 6
-            arrived: 4,
-            capacity: 7,
-            done: 5,
-            backlogSizeAfter: 0,
-        },
+        { arrived: 5, capacity: 5, done: 5, backlogSizeAfter: 0 }, // iteration 1
+        { arrived: 5, capacity: 4, done: 4, backlogSizeAfter: 1 }, // iteration 2
+        { arrived: 6, capacity: 5, done: 5, backlogSizeAfter: 1 }, // iteration 3
+        { arrived: 6, capacity: 6, done: 6, backlogSizeAfter: 0 }, // iteration 4
+        { arrived: 4, capacity: 5, done: 4, backlogSizeAfter: 0 }, // iteration 5
     ];
 
     const unboundedBacklog = computeBacklogBehaviour(iterationArrivals, iterationCapacities);
     const boundedBacklog = computeBacklogBehaviour(iterationArrivals, iterationCapacities, 6);
 
-    console.log(unboundedBacklog, unboundedBacklogExpectation)
+    console.log(unboundedBacklog, boundedBacklog)
 
     console.log("correctly computes unbounded backlog behaviour", isDeepEqual(unboundedBacklog, unboundedBacklogExpectation));
     console.log("correctly computes bounded backlog behaviour", isDeepEqual(boundedBacklog, boundedBacklogExpectation));
--- writings/backlog-growth-illustrations.js
+++ writings/backlog-growth-illustrations.js
@@ -4,12 +4,10 @@ function computeBacklogBehaviour(arrivals, capacities, wipLimit) {
     let backlogSize = 0;
 
     arrivals.forEach((arrived, index) => {
-        //backlogSize = Math.min(backlogSize + arrived, backlogWipLimit);
-
-        backlogSize = backlogSize + arrived;
+        backlogSize = Math.min(backlogSize + arrived, backlogWipLimit);
 
         const capacity = capacities[index];
-        const done = (capacity > backlogSize) ? capacity : backlogSize;
+        const done = (capacity > backlogSize) ? backlogSize : capacity;
 
         backlogSize = backlogSize - done;
 
@@ -26,70 +24,11 @@ function computeBacklogBehaviour(arrivals, capacities, wipLimit) {
     return backlogBehaviour;
 };
 
-
 (function () {
+    const iterationArrivals = [5, 5, 6, 6, 5];
+    const iterationCapacities = [5, 4, 4, 6, 6];
+    const wipLimit = 6;
 
-    const iterationArrivals = [
-        5,
-        5,
-        6,
-        6,
-        4,
-        4,
-    ];
-
-    const iterationCapacities = [
-        5,
-        4,
-        5,
-        6,
-        5,
-        7,
-    ];
-
-    const unboundedBacklog = [
-        {
-            // iteration 1
-            arrived: 5,
-            capacity: 5,
-            done: 5,
-            backlogSizeAfter: 0,
-        },
-        {
-            // iteration 2
-            arrived: 5,
-            capacity: 4,
-            done: 4,
-            backlogSizeAfter: 1,
-        },
-        {
-            // iteration 3
-            arrived: 6,
-            capacity: 6,
-            done: 6,
-            backlogSizeAfter: 3, // 2,
-        },
-        {
-            // iteration 4
-            arrived: 6,
-            capacity: 6,
-            done: 6,
-            backlogSizeAfter: 3,
-        },
-        {
-            // iteration 5
-            arrived: 4,
-            capacity: 5,
-            done: 5,
-            backlogSizeAfter: 2, // 1,
-        },
-        {
-            // iteration 6
-            arrived: 4,
-            capacity: 7,
-            done: 6, // 5,
-            backlogSizeAfter: 0, // 0,
-        },
-    ];
-
+    const unboundedBacklog = computeBacklogBehaviour(iterationArrivals, iterationCapacities);
+    const boundedBacklog = computeBacklogBehaviour(iterationArrivals, iterationCapacities, wipLimit);
 })();
