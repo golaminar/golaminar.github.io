f76f0d311e1eb4bb3cebcaa0cc05f83c0998016c
Tom Stuart <tom@golaminar.de>
2022-12-01 15:12:19 +0100

    WIP of charts for CoD
    Acronyms are cool
    
    Co-authored-by: Tiffany Conroy <theophani@users.noreply.github.com>

---
+++ writings/cost-of-delay-illustrations.js
@@ -0,0 +1,49 @@
+const generateCostOfDelayChart = function (id, dataSets) {
+    const labels = [1, 2, 3, 4];
+    const chartData = {
+        labels: labels,
+        datasets: dataSets.map ((data) => {
+          return {
+              color: "pink",
+              data: data.reduce((ary, val) => {
+                //data = [500, 500, 0, 0] => ary = [500, 1000, 1000, 1000]
+                ary.push(val + (ary.length ? ary.at(-1) : 0));
+                return ary;
+              }, []),
+          };
+        })
+    };
+
+    const config = {
+        type: 'bar',
+        data: chartData,
+        options: {
+            scales: {
+                x: {
+                    stacked: true,
+                },
+                y: {
+                    stacked: true,
+                }
+            }
+        }
+    };
+
+    new Chart(document.getElementById(id), config);
+};
+
+(function() {
+    const item1Costs = [0, 0, 0, 0];
+    const item2Costs = [500, 0, 0, 0]; 
+    const item3Costs = [500, 500, 0, 0]; 
+    const item4Costs = [500, 500, 500, 0]; 
+    const item5Costs = [500, 500, 500, 500];
+
+    generateCostOfDelayChart('item-1-cost-of-delay-chart', [item1Costs]);
+    generateCostOfDelayChart('item-2-cost-of-delay-chart', [item2Costs]);
+    generateCostOfDelayChart('item-3-cost-of-delay-chart', [item3Costs]);
+    generateCostOfDelayChart('item-4-cost-of-delay-chart', [item4Costs]);
+    generateCostOfDelayChart('item-5-cost-of-delay-chart', [item5Costs]);
+
+    generateCostOfDelayChart('cost-of-delay-cumulative-chart', [item1Costs, item2Costs, item3Costs, item4Costs, item5Costs]);
+})();
